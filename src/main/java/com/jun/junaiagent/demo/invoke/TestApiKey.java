2ec4513315eaaf4078ed89f02d83091fc4b5f9e9
huangyongjun <347652590@qq.com>
2026-01-12 18:47:07 +0800

    新增sdk测试调用大模型方式

---
+++ src/main/java/com/jun/junaiagent/demo/invoke/TestApiKey.java
@@ -0,0 +1,10 @@
+package com.jun.junaiagent.demo.invoke;
+
+/**
+ * 仅用于测试获取 API Key
+ */
+public interface TestApiKey {
+
+    // 修改为你的 API Key
+    String API_KEY = "sk-ed20fbe594394349a9d2fe94cc320469";
+}
