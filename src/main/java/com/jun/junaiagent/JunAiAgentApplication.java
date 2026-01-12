f68465ef9ed535f5c62e1a700acab458d7aa5aed
huangyongjun <347652590@qq.com>
2026-01-12 10:35:48 +0800

    first commit

---
+++ src/main/java/com/jun/junaiagent/JunAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.jun.junaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jun/junaiagent/JunAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jun.junaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
